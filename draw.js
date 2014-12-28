fd53758e03b8419ee3e836f1e46892ae928a03fe
whatwehaveunlearned <agon@hawaii.edu>
2014-12-28 01:01:09 +0100

    Created basic ScatterPlot

--- draw.js
+++ draw.js
@@ -1,11 +1,11 @@
-//For Testing purposes Random Generator
+/*//For Testing purposes Random Generator
 var dataset = [];
 var Size =25;
 for (var i=0;i<Size;i++){
 	var newNumber = Math.round(Math.random() * 30);
 	dataset.push(newNumber);
-}
-
+}*/
+var dataset = [[5, 20], [480, 90], [250, 50], [100, 33], [330, 95], [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]];
 //Define margin and Size of container
 var margin = {
     top: 20,
@@ -22,7 +22,7 @@ main();
 //Main Function
 function main (){
 
-	simpleCol();
+	scatterPlot();
 	
 	//Viz Types Functions
 	//simpleCol
@@ -61,6 +61,83 @@ function main (){
 	        "text-anchor":"middle"
 	      });          
 
+	}
+
+	function scatterPlot(){
+	  var padding = 30;
+	  //variable to format ticks
+	  //var formatAsPercentage = d3.format(".1%");
+	  //Scale
+	  var xScale = d3.scale.linear()
+	  				 .domain([0,d3.max(dataset, function(d) { return d[0]; } )])
+	  				 .range([padding,w-padding]);
+	  var yScale = d3.scale.linear()
+	  				 .domain([0, d3.max(dataset, function(d) { return d[1]; } )])
+	  				 .range([h-padding,padding]);
+	  var rScale = d3.scale.linear()
+	  				 .domain([0, d3.max(dataset, function(d) { return d[1]; } )])
+	  				 .range([2,5]);
+	  //Axis
+	  var xAxis = d3.svg.axis()
+	  					.scale(xScale)
+	  					.orient("bottom")
+	  					.ticks(10);
+	  var yAxis = d3.svg.axis()
+	  					.scale(yScale)
+	  					.orient("left")
+	  					.ticks(10);
+	  					//.tickFormat(formatAsPercentage);
+	  
+	  //svg element
+	  var svg = d3.select("#chartArea")
+	              .append("svg")
+	              .attr("width",w)
+	              .attr("height",h);
+	  
+	  //Draw scatterplot
+	  svg.selectAll("circle")
+	     .data(dataset)
+	     .enter()
+	     .append("circle")
+	     .attr({
+	        cx: function(d,i){ return xScale(d[0]); },
+	        cy: function(d) { return yScale(d[1])},
+	        r: function(d) { return rScale(d[1])},
+	        height: function (d) {return d*4;},
+	        fill: function(d) { return "rgb(0,0,"+(d*10)+")";}
+	      });
+	  
+	  //Draw Text
+	  svg.selectAll("text")
+	     .data(dataset)
+	     .enter()
+	     .append("text")
+	     .text(function(d){
+	        return d[0] + "," + d[1];
+	     }) 
+	     .attr({
+	        x: function(d,i){ return xScale(d[0]); },
+	        y: function(d) { return yScale(d[1])}, 
+	        "font-family": "sans-serif",
+	        "font-size": 11,
+	        fill: "red",
+	        "text-anchor":"middle"
+	      });
+	   
+	   //Draw Axis
+	   svg.append("g")
+	   	  .attr({
+	   	  	class: "axis",
+	   	  	"transform": "translate(0," + (h-padding) + ")"
+	   	  })
+	   	  .call(xAxis);
+
+	   	svg.append("g")
+	   	  .attr({
+	   	  	class: "axis",
+	   	  	"transform": "translate(" + (padding) + ",0)"
+	   	  })
+	   	  .call(yAxis);          
 	}
 	//Colviz
 	function colviz (){
